34a9a156d28a17e61f77646940e6f1cb256355c0
Alan <m.limaalan@hotmail.fr>
2019-01-09 16:22:41 +0100

    add the getUsers() function on init for administration

--- src/app/administration/administration.page.ts
+++ src/app/administration/administration.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.page.html',
   styleUrls: ['./administration.page.scss'],
 })
-export class AdministrationPage implements OnInit {
+export class AdministrationPage implements OnInit
+{
 
-  constructor() { }
+  list: any[] = [];
 
-  ngOnInit() {
+  constructor(private userService: UserService) { }
+
+  ngOnInit()
+  {
+    this.getUsers();
   }
 
+  getUsers()
+  {
+    this.userService.getUsers().subscribe(data => {
+      let cle = Object.keys(data);
+      let donnees = Object.values(data);
+      for (let i = 0; i < cle.length; i++)
+      {
+        this.list.push({ key: cle[i], values: donnees[i] });
+      }
+    });
+  }
+
+  increase(key: string){  }
+  decrease(key: string){  }
 }
