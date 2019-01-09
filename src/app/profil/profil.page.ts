c9d9a829b57e702c75b483251a0cc8c7d7d03261
Alan <m.limaalan@hotmail.fr>
2019-01-09 16:23:44 +0100

    add the first part of the informations page and functions tu retrieve a user

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { OrderService } from '../service/order.service';
+import { User } from "../class/user";
+import { Order } from "../class/order";
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
-  constructor() { }
+  user:   User;
+  list:   any[] = [];
+  order:  Order[];
+
+  constructor(private userService: UserService, private orderService: OrderService) { }
 
   ngOnInit() {
+    // Allow us to by default charge an example user. 
+    this.user = this.userService.getUserByKey("-LT7EJHxqq4Zycn7lX36")[0];
+    this.getOrders();
   }
 
+  getOrders() {
+    this.orderService.getOrders().subscribe(data => {
+      let cle = Object.keys(data);
+      let donnees = Object.values(data);
+      for (let i = 0; i < cle.length; i++) {
+        this.list.push({ key: cle[i], values: donnees[i] });
+      }
+    });
+  }
 }
