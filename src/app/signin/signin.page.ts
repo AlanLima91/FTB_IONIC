557cfbbd5b1470003fafc0edb627df382c8ac5ab
Alan <m.limaalan@hotmail.fr>
2019-01-09 16:21:42 +0100

    add the function onSubmit(form), but the function is still in progress

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
 })
-export class SigninPage implements OnInit {
+export class SigninPage implements OnInit{
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form)
+  {
+    /* this.beerService.editBeer(form.form.value, this.key).subscribe(beer => {
+      this.editForm();
+      this.router.navigateByUrl('');
+    }); */
+    console.log("You pushed the connection button, Well Done !");
+  }
 }
