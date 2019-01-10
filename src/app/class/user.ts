1757275940a34716e6ef386155d61feb48eb0bed
Alan <m.limaalan@hotmail.fr>
2019-01-10 12:13:37 +0100

    normalisation code

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -1,12 +1,13 @@
 import { Order } from './order';
 
-export class User {
-    public name: string;
-    public firstname: string;
-    public email: string;
-    public password: string;
-    public solde: number;
-    public order: Order[] | void;
+export class User
+{
+    public name:        string;
+    public firstname:   string;
+    public email:       string;
+    public password:    string;
+    public solde:       number;
+    public order:       Order[] | void;
 
     constructor(name: string, firstname: string, email: string, password: string, solde: number, order: Order[] | void) {
         this.name = name;
