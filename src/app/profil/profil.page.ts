2c7b0fa88e7bef12372cf60b5dd6207e16d5cb98
Alan <m.limaalan@hotmail.fr>
2019-01-11 09:43:17 +0100

    implements an alert help window

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
 import { OrderService } from '../service/order.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profil',
@@ -12,7 +13,7 @@ export class ProfilPage implements OnInit {
   user:   Object;
   list:   any[] = [];
 
-  constructor(private userService: UserService, private orderService: OrderService) { }
+  constructor(private userService: UserService, private orderService: OrderService, private alertController: AlertController) { }
 
   /*
    *  First you load an user by his key.
@@ -36,10 +37,7 @@ export class ProfilPage implements OnInit {
        *  de la dernière à la première. (Mais il y a un coté Aléatoire dans mes tests.) 
        */
       while (i != -1)
-      {
-        console.log(this.user['order'][i])
         this.getOrderByKey(this.user['order'][i--]);
-      }
     });
   }
   
@@ -48,5 +46,20 @@ export class ProfilPage implements OnInit {
     this.orderService.getOrderByKey(key).subscribe(data => {
       this.list.push(data);
     });
+  }
+
+  async onClick()
+  {
+    const alrt = await this.alertController.create({
+      header: 'Besoin d\'aide ?',
+      message: 'Si vous souhaitez changer d\'adresse mail ou de mot de passe, vous devez simplement cliquer sur votre adresse mail ou le "mot de passe" et saisir les nouvelles valeurs.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => { }
+        }
+      ]
+    });
+    await alrt.present();
   }
 }
