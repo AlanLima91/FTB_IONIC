40b921f70c57d15864f73fb429bdc9e8ab8c9e26
Alan <m.limaalan@hotmail.fr>
2019-01-23 12:02:50 +0100

    add form to signup page and handle data

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class SignupPage implements OnInit
+{
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
 
-  ngOnInit() {
+  ngOnInit()
+  {  }
+
+  /**
+   * 
+   * @param form 
+   * 
+   * Prend un formulaire en paramètre, l'ajoute à la table User
+   * et redirige vers la page de connexion.
+   * Le mot de passe n'est pas crypté tout est en clair !
+   */
+  onSubmit(form)
+  {
+    console.log(form.form.value);
+    this.userService.addUser(form.form.value).subscribe(user => {
+      this.router.navigate(['./signin']);
+    })
   }
-
 }
