d8809f4dd8abbe2703ace2657bdd00f63f57fce4
Alan <m.limaalan@hotmail.fr>
2019-02-06 19:51:56 +0100

    change order type for array of string

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -3,11 +3,11 @@ export class User
     public email:       string;
     public firstname:   string;
     public name:        string;
-    public order:       any[] | void;
+    public order:       string[] | void;
     public password:    string;
     public solde:       number;
 
-    constructor(email: string, firstname: string, name: string, order: any[] | void, password: string, solde: number)
+    constructor(email: string, firstname: string, name: string, order: string[] | void, password: string, solde: number)
     {
         this.name = name;
         this.firstname = firstname;
