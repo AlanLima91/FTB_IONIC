f240e1a29a7105323c00791160f5f48655f3898c
Alan <m.limaalan@hotmail.fr>
2019-02-06 19:46:41 +0100

    we can access to the header of the response of login

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
 import { Router } from '@angular/router';
+import { User } from '../class/user';
 
 @Component({
   selector: 'app-signin',
@@ -9,15 +10,32 @@ import { Router } from '@angular/router';
 })
 export class SigninPage implements OnInit
 {
+  headers : any;
 
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit()
-  {  }
+  { 
+    console.log("MAIL@mail.fr");
+    console.log("password");
+  }
 
   onSubmit(form)
   {
-    console.log("You pushed the connection button, Well Done !");
-    
+    let user = new User(
+      form.form.value.email,
+      form.form.value.firstname,
+      form.form.value.name,
+      null, // Order
+      form.form.value.password,
+      0     //solde
+    );
+
+    this.userService.loginUser(user).subscribe(resp => {
+      console.log(resp.headers.has('x-auth'));
+    },
+    err => {
+      console.log(err);
+    });
   }
 }
