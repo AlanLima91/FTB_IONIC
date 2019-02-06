c7910a967b9105a36d03d688de707e8aed6f4af4
Alan <m.limaalan@hotmail.fr>
2019-02-06 19:49:58 +0100

    the signup forms is working

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -18,35 +18,10 @@ export class SignupPage implements OnInit
 
   /**
    * 
-   * @param user 
-   * 
-   * Prend un User en parametre.
-   * Le but ici est de comparé que l'email saisie dans l'inscription n'est pas deja en base
-   * s'il est en base la fonction renvoie un false
-   * s'il n'est pas en base 
-   */
-  checkBeforeSignUp(user)
-  {
-    this.userService.getUsers().subscribe(data => {
-      let users: User[] = Object.values(data);
-      users.filter(user => {
-        for (let i = 0; i < users.length; i++)
-        {
-          if (user.email === users[i].email)
-            return true;
-        }
-        return false;
-      })
-    })
-  }
-
-  /**
-   * 
    * @param form 
    * 
    * Prend un formulaire en paramètre, l'ajoute à la table User
    * et redirige vers la page de connexion.
-   * Le mot de passe n'est pas crypté tout est en clair !
    */
   onSubmit(form)
   {
@@ -58,12 +33,9 @@ export class SignupPage implements OnInit
       form.form.value.password,
       0     //solde
     );
- 
-    if (!this.checkBeforeSignUp(user))
-    {
-      this.userService.addUser(user).subscribe(user => {
-        this.router.navigate(['./signin']);
-      })
-    }
+
+    this.userService.addUser(user).subscribe(user => {
+      this.router.navigate(['./signin']);
+    });
   }
 }
